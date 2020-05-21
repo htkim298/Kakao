23b89d999a0f4771102232726dde02f92c53775f
Michael <htkim298@gmail.com>
2020-05-21 09:38:57 +0900

    테스트

--- kakao/blog/views.py
+++ kakao/blog/views.py
@@ -27,6 +27,32 @@ def message(request):
                     'label': '처음으로',
                     'action': 'message',
                     'messageText': '처음으로'
-                }]
+                }],
+                "action": {
+                    "name": "예제 스킬",
+                    "id": "...",
+                    "params": {
+                      "sys_date": "{\"dateTag\": \"Friday\", \"dateHeadword\": null, \"month\": null, \"year\": null, \"date\": \"2018-11-23\", \"day\": null}",
+                      "sys_location": "강남",
+                      "sys_number": "{\"amount\": 1, \"unit\": null}"
+                    },
+                    "detailParams": {
+                      "sys_date": {
+                        "origin": "금요일",
+                        "value": "{\"dateTag\": \"Friday\", \"dateHeadword\": null, \"month\": null, \"year\": null, \"date\": \"2018-11-23\", \"day\": null}",
+                        "groupName": ""
+                      },
+                      "sys_location": {
+                        "origin": "강남",
+                        "value": "강남",
+                        "groupName": ""
+                      },
+                      "sys_number": {
+                        "origin": "1",
+                        "value": "{\"amount\": 1, \"unit\": null}",
+                        "groupName": ""
+                      }
+                    }
+                  }
             }
         })
