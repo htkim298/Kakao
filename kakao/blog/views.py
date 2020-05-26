441ce62e8a129da9cf6f0852440cb6ce50150dc1
Michael <htkim298@gmail.com>
2020-05-26 12:13:06 +0900

    테스트

--- kakao/blog/views.py
+++ kakao/blog/views.py
@@ -14,6 +14,18 @@ def message(request):
     return_json_str = json.loads(answer)
     return_str = return_json_str['userRequest']['utterance']
 
+    if return_str == '사용법':
+        return JsonResponse({
+            'version': "2.0",
+            'template': {
+                'outputs': [{
+                    'simpleText': {
+                        'text': "HELP_TEXT"
+                    }
+                }]
+            })
+
+
     if return_str == '카카오':
         return JsonResponse({
             'version': "2.0",
@@ -28,36 +40,5 @@ def message(request):
                     'action': 'message',
                     'messageText': '처음으로'
                 }]
-                },
-                "context": {
-                    "values": [
-                      {
-                        "name": "abc",
-                        "lifeSpan": 10,
-                        "params": {
-                          "key1": "val1",
-                          "key2": "val2"
-                        }
-                      },
-                      {
-                        "name": "def",
-                        "lifeSpan": 5,
-                        "params": {
-                          "key3": "1",
-                          "key4": "true",
-                          "key5": "{\"jsonKey\": \"jsonVal\"}"
-                        }
-                      },
-                      {
-                        "name": "ghi",
-                        "lifeSpan": 0
-                      }
-                    ]
-                  },
-                  "data": {
-                    "msg":"HI",
-                    "name":"Ryan",
-                    "position":"Senior Managing Director"
-                  }
-
-        })
+                }
+            })
